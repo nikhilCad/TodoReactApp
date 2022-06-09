0027085fc94ec44d74fc58a366c2c23390b14a12
nikhilcad <nikhilcaddilac@gmail.com>
2022-06-09 14:05:39 +0530

    Remove seach bar

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
    
-    <Header title="My Todos List" searchBar={true}/>
+    <Header title="My Todos List" searchBar={false}/>
     <Todos/>
     <Footer/>
 
--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -28,7 +28,7 @@ export default function Header(props) { //use as {props.var1} {props.var2} etc i
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
-      : "No search bar"}
+      : ""}
 
     </div>
   </div>
