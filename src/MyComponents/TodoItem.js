49f7ad13329ff4cd89082c85fcffb2cd457f15d8
nikhilcad <nikhilcaddilac@gmail.com>
2022-06-09 17:47:51 +0530

    Use arrow func in onDelete so dont call by itself on start

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -6,12 +6,13 @@ import React from 'react'
 
 // we wrote {todo, onDelete} in below so not write {props.todo} everytime
 //onDelete called when button clicked
+//we use arrow function in onClick so it dont call by itslef on startup, function is PASSED not CALLED
 export const TodoItem = ({todo, onDelete}) => {
     return (
         <div>
             <h4>{todo.title}</h4>
             <p>{todo.desc}</p>
-            <button className="btn btn-sm btn-danger" onClick={onDelete(todo)}>Delete</button>
+            <button className="btn btn-sm btn-danger" onClick={()=>{onDelete(todo)}}>Delete</button>
         </div>
     )
 }
