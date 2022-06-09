cf8718ce386e6518e74b925c88e0f204693e0bf9
nikhilcad <nikhilcaddilac@gmail.com>
2022-06-09 12:15:51 +0530

    parameters in App.js, props, conditional element

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './MyComponents/Header';
 import {Todos} from './MyComponents/Todos'; //use {} if using rafc as it is trying to find a default export for in your file.
 import {Footer} from './MyComponents/Footer';
-
+ 
 function App() {
   let myVariable = 345;
   return (
