e4c1a8d3e2a1fd0d4e9e8c3ddec0d979ac65ce99
nikhilcad <nikhilcaddilac@gmail.com>
2022-06-11 15:24:13 +0530

    Extremely buggy save load

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ import React, { useState } from 'react';
  
 function App() {
 
+  let initTodo;
+  //load items, we later pass initTodo in our state hook at around line 56
+  if(localStorage.getItem("todos")===null){
+    initTodo =[];
+    
+  }else{
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  };
+
   //arrow function of javascript, called from TodoItem.js 
   const onDelete =(todo)=>{
       console.log("I am ondelete of todo", todo);
@@ -16,7 +25,8 @@ function App() {
       setTodos(todos.filter((e)=>{
         return e!==todo; //delete this todo
       }
-      ))
+      ));
+      localStorage.setItem("todos",JSON.stringify(todos));//save it
   }
 
   const addTodo =(title,desc)=>{
@@ -37,12 +47,13 @@ function App() {
     console.log(myTodo);
     //use setTodos to add new todo in the full list/array 
     setTodos([...todos,myTodo]);//... spreads the array elements, so this line is todo1,todo2,...,myTodo
+
+    localStorage.setItem("todos",JSON.stringify(todos));//save it
+
   }
   
-  //setTodos a function that updates the todo
-  const [todos, setTodos] = useState([
-
-  ]);
+  //setTodos a function that updates the todo, initTodo is the load item
+  const [todos, setTodos] = useState(initTodo);
 
   return (
     <>
